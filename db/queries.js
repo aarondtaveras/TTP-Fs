a5133b0d71a56dd012823b42868cb3521ebca11b
ADT <aarondtaveras@gmail.com>
2019-04-23 12:21:45 -0400

    Filling in & testing queries, updating views

--- db/queries.js
+++ db/queries.js
@@ -16,17 +16,82 @@ const pool = new Pool({
     user: 'aaron',
     host: 'localhost',
     database: 'ttp',
-    password: 'password'
+    password: 'adt62497'
 })
 
+async function getUser(id){
+    var query = {
+        text: 'SELECT * FROM users WHERE user_id=$1',
+        values: [id]
+    }
+    pool.query(query)
+    .catch(e => {
+        throw new Error(e);
+    })
+    return {message: "Success!"};
+}
+
+async function getTransactions(id){
+
+}
+
+async function getOwnedStocks(id){
+
+}
+
+async function createUser(body){
+    // Our database has a unique constraint on our emails, so if there are duplicates we will catch the error and throw a new one.
+    var query = {
+        text: 'INSERT INTO users(name,password,email) VALUES($1,$2,$3)',
+        values: [body.name,body.password,body.email]
+    }
+    pool.query(query)
+    .catch(e => {
+        throw new Error(e);
+    });
+    return {message: "Success!"};
+}
+
+// Log the user in by checking for the correct username and password.
+async function loginUser(body){
+    let msg = "Success!";
+    var query = {
+        text: 'SELECT * from users WHERE email=$1 AND password=$2',
+        values: [body.email,body.password]
+    }
+    pool.query(query)
+    .then(res => {
+        if(!res.rowCount){
+            msg = "Invalid email/password.";
+            // throw new Error("Invalid email/password.");
+        }
+    })
+    return {message: msg};
+}
+
+// User balances can only decrease since we do not allow for selling of stock yet.
+async function updateUserBalances(id, amount){
+    var query = {
+        text: 'UPDATE users SET balance=balance-$1 WHERE user_id=$2',
+        values: [amount,id]
+    }
+    pool.query(query)
+    .catch(e => {
+        throw new Error(e);
+    })
+    return {message: "Success!"};
+}
+
+async function buyStock(id, ticker, quantity){
+
+}
 
 module.exports = {
-    // getUser,
-    // getTransactions,
-    // getOwnedStocks,  
-    // createUser,
-    // loginUser,
-    // updateUserBalances,
-    // updateUserEmail,
-    // updateUserPassword
+    getUser,
+    getTransactions,
+    getOwnedStocks,  
+    createUser,
+    loginUser,
+    updateUserBalances,
+    buyStock
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,12 +10,25 @@ router.get('/', async(req,res) => {
 });
 
 router.post('/',async(req,res)=> {
+  try{
+    let response = await queries.loginUser(req.body);
+  } catch(err){
+    res.render('/',{title: err})
+  }
   res.render('index', {title: 'Stock App'});
-
 })
 
 router.get('/register', async(req,res) => {
   res.render('register', {title: 'Stock App'})
+})
+
+router.post('/register', async(req,res)=> {
+  try {
+    let response = await queries.createUser(req.body);
+  } catch(err){
+    res.render('register', {title: err});
+  }
+  res.redirect('/');
 })
 
 module.exports = router;
